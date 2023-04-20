0e4a3ca775f130122fb827cf3cfebeb9297694ee
likitarai2001 <likita.r@simformsolutions.com>
2023-04-20 11:02:40 +0530

    Add src folder

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        String str1 = " Simform ";
+
+        StringBuilder str2 = new StringBuilder(" The quick"); //The quick brown fox jumps over the lazy dog
+        str2.append(" brown")
+                .append(" fox jumps")
+                .append(" over the")
+                .append(" lazy dog. ");
+
+        StringBuffer str3 = new StringBuffer(" Sphinx");
+        str3.append(" of black quartz,")
+                .append(" judge")
+                .append(" my ")
+                .append("vow.");
+
+        System.out.println(str1.toUpperCase().trim());
+        System.out.println(str2.toString().toUpperCase().trim());
+        System.out.println(str3.toString().toUpperCase().trim());
+    }
+}
